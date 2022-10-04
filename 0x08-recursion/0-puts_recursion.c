3324f2d15e1e9cd03368f0c451a8ad5070242782
Reginald-kyalo <reginald.kyalo@gmail.com>
2022-10-04 05:05:06 -0700

    corrected to follow betty style

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
-/*
+/**
  * _puts_recursion - prints a string
  * @s: pointer block of a memory to fill
  * Return: void
  */
 
-void _puts_recursion (char *s)
+void _puts_recursion(char *s)
 
 {
 	if (*s == '\0')
@@ -15,6 +15,6 @@ void _puts_recursion (char *s)
 		return;
 	}
 	_putchar (*s);
-	_puts_recursion (s + 1);
+	_puts_recursion(s + 1);
 
 }
