c7995229ed3fba0f46d8f80f42599f2fab7829bf
Reginald-kyalo <reginald.kyalo@gmail.com>
2023-02-26 13:56:23 -0800

    added file

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -15,19 +15,12 @@ void more_numbers(void)
 	{
 		for (j = 0; j < 15; j++)
 		{
-			if (j < 10)
-			{
-				_putchar(j + 48);
-			}
-			else if (j > 9)
-			{
-				_putchar((j / 10) + 48);
+			if (j > 9)
+				_putchar('1');
 				_putchar((j % 10) + 48);
-			}
-
 		}
 		_putchar('\n');
 
 	}
-	_putchar('\n');
+
 }
