bba55cebcee2d92a0b2d344a3aa8d957b1eefad4
Reginald-kyalo <reginald.kyalo@gmail.com>
2023-02-26 15:10:51 -0800

    added file

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -11,18 +11,15 @@ void print_square(int size)
 {
 	int i, j;
 
+	if (size <= 0)
+		_putchar('\n');
+
 	for (i = 0; i < size; i++)
 	{
 		for (j = 0; j < size; j++)
 		{
-			if (size > 0)
-			{
-				_putchar('#');
-			}
-			else
-			{
-				_putchar('\n');
-			}
+			_putchar('#');
+
 		}
 		_putchar('\n');
 	}
