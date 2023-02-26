00ff465ed08a8c24776b3fa7384f44cf25468d24
Reginald-kyalo <reginald.kyalo@gmail.com>
2023-02-26 13:43:17 -0800

    added file

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * most_numbers - prints 0 - 14 ten times followed by a new line
+ *
+ * Return: nothing
+ */
+
+void most_numbers(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j < 15; j++)
+		{
+			if (j < 10)
+			{
+				putchar(j + 48);
+			}
+			else if (j > 9)
+			{
+				putchar((j / 10) + 48);
+				putchar((j % 10) + 48);
+			}
+
+		}
+		putchar('\n');
+
+	}
+	putchar('\n');
+}
