5b076f217fcb2ae5779ca391ec993e21495c307a
Reginald-kyalo <reginald.kyalo@gmail.com>
2023-03-02 04:51:13 -0800

    added file

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -2,38 +2,38 @@
 
 /**
  * _strcmp - compares two strings
- * @dest: string to be compared
- * @src: with this
+ * @s1: string to be compared
+ * @s2: with this
  *
  * Return: 15 if dest is bigger than src, -15 the other way
  * and 0 if they are equal in length
  */
 
-int _strcmp(char *dest, char *src)
+int _strcmp(char *s1, char *s2)
 {
 	int i = 0;
 	int j = 0;
-	int dest_len = 0;
-	int src_len = 0;
+	int s1_len = 0;
+	int s2_len = 0;
 
-	while (src[i++])
+	while (s2[i++])
 	{
-		src_len++;
+		s2_len++;
 	}
-	while (dest[j++])
+	while (s1[j++])
 	{
-		dest_len++;
+		s1_len++;
 	}
 
-	if (dest_len > src_len)
+	if (s1_len > s2_len)
 	{
 		return (15);
 	}
-	else if (src_len > dest_len)
+	else if (s2_len > s1_len)
 	{
 		return (-15);
 	}
-	else if (src_len == dest_len)
+	else if (s2_len == s1_len)
 	{
 		return (0);
 	}
