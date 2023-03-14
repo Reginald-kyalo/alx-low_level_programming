191c4bf7f2aa1f8b92a4f7d62598979ccce386d4
Reginald-kyalo <reginald.kyalo@gmail.com>
2023-03-14 14:00:08 -0700

    added file

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - duplicates array
+ * @str: array to be duplicated
+ *
+ * Return: array
+ */
+
+char *_strdup(char *str)
+{
+	int i = 0;
+	int str_len = 0;
+	char *array;
+
+	while (str[i])
+	{
+		i++;
+		str_len++;
+	}
+	array = malloc(str_len * sizeof(char));
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	if (str_len != 0)
+	{
+		while (i <= str_len)
+		{
+
+			array[i] = str[i];
+			i++;
+		}
+		array[i] = '\0';
+		return (array);
+	}
+	return (NULL);
+}
