cebcb7351897b5495b3677c35ce5e230ffa67a43
Reginald-kyalo <reginald.kyalo@gmail.com>
2023-03-15 11:01:37 -0700

    added file

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - prints number if command line arguments passed
+ *
+ * Return: pointer to argv
+ */
+int main(int argc, char  *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
