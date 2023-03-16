5c0722830fd6f7496d011db89c1f4a81047176d8
Reginald-kyalo <reginald.kyalo@gmail.com>
2023-03-16 14:33:30 -0700

    added file

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _calloc - if malloc placed themselves in calloc's shoes
+ * @nmemb: number of elements
+ * @size: size of elements
+ *
+ * Return: pointer on success, NULL on failure
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int i;
+	char *array;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	array = malloc(nmemb * size);
+
+	if (array == NULL)
+		return (NULL);
+
+	for (i = 0; i < nmemb; i++)
+		array[i] = 0;
+
+	return (array);
+}
